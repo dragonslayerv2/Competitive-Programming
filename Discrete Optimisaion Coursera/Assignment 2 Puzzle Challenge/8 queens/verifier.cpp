a1d2773e0b6c854d5ad2d927aaeb815f36cf1dbf
Shobhit Saxena <shobhitsaxena@live.com>
2014-03-12 09:53:52 +0530

    n queens verifier updated

--- Discrete Optimisaion Coursera/Assignment 2 Puzzle Challenge/8 queens/verifier.cpp
+++ Discrete Optimisaion Coursera/Assignment 2 Puzzle Challenge/8 queens/verifier.cpp
@@ -4,28 +4,65 @@
 #include<vector>
 using namespace std;
 
-vector<vector<bool> > board;
+vector<int> board;
 
+#define isSafe(x1,y1,x2,y2) (x1!=x2&&y1!=y2&&(y1-y2)!=(x1-x2)&&(y1-y2)!=(x2-x1))
 int main(int argc,char *argv[])
 {
 	
-	if(argc==3)
+	if(argc==4)
 	{
 		ofstream log(argv[2]);
 		for(int n=4;n<100;n++)	
 		{
-			cout<<"---------------------------------"<<endl;
 			char command[1000];
-			sprintf(command,"%s %d",argv[1],n);
+			sprintf(command,"%s %d >%s",argv[1],n,argv[3]);
+		
+		
 			clock_t start,end;
 			start=clock();
 			system(command);
 			end=clock();
-			double time=((float)(end-start))/CLK_TCK;
-			cout<<endl<<time<<endl;
-			log<<n<<" "<<time<<endl;
-			log.flush();
+			double time=((double)(end-start))/CLK_TCK;
+
+		
+			ifstream input(argv[3]);
+			int N;
+			input>>N;
+			bool isOK=true;
+			if(n!=N)
+				isOK=false;
+			else
+			{
+				board.resize(n);
+				for(int i=0;i<n;i++)
+					input>>board[i];
+				
+				for(int i=0;i<n;i++)
+					for(int j=0;j<n;j++)
+						if(i!=j)
+							if(!isSafe(i,board[i],j,board[j]))
+								isOK=false;
+			}
+
+			cout<<n<<"\t=>\t";
+			log <<n<<"\t=>\t";
+			if(isOK)
+			{
+				cout<<"Verified OK\t"<<time<<endl;
+				log<<"Verified OK\t"<<time<<endl;
+				log.flush();
+			}
+			else
+			{
+				cout<<"Wrong answer"<<endl;
+				log <<"Wrong answer"<<endl;
+			}
+				
 			cout<<"-----------------------------------------"<<endl;
+			log <<"-----------------------------------------"<<endl;
 		}
 	}
+	else 
+		cout<<"Usage: verifier [exec_file_name] [log_file_name] [output_dmp_file]\n";
 }
